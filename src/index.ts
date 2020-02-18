7f7cf3241906b47bc1c44eb4ce09aac21e2980cc
benada002 <45796304+benada002@users.noreply.github.com>
2020-02-18 15:36:05 +0100

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
 interface IPluginOptions {
-    extensions?: string | Array<string>,
-    onlyOnWrite?: boolean
+  extensions?: string | Array<string>,
+  onlyOnWrite?: boolean
 }
 
-export default (options: IPluginOptions) => {
-    const name = 'rollup-non-js-entry';
-    const extensions = options.extensions ?? ['.css', '.styl', '.scss', '.sass', '.less']
-    const onlyOnWrite = options.onlyOnWrite ?? false;
+export default (pluginOptions: IPluginOptions) => {
+  const name = 'rollup-non-js-entry';
+  const extensions = pluginOptions.extensions ?? ['.css', '.styl', '.scss', '.sass', '.less'];
+  const onlyOnWrite = pluginOptions.onlyOnWrite ?? false;
 
-    return {
-        name,
-        options(options) {
-            const { plugins } = options;
-            const pluginIndex = plugins.findIndex(plugin => plugin.name === name);
+  return {
+    name,
+    options(options) {
+      const { plugins } = options;
+      const pluginIndex = plugins.findIndex((plugin) => plugin.name === name);
 
-            if (pluginIndex !== -1) {
-                const pluginElement = plugins.splice(pluginIndex, 1)[0]
-                plugins.push(pluginElement);
+      if (pluginIndex !== -1) {
+        const pluginElement = plugins.splice(pluginIndex, 1)[0];
+        plugins.push(pluginElement);
+      }
+
+      return options;
+    },
+    async generateBundle(options, bundle, isWrite) {
+      const keys = Object.keys(bundle);
+
+      if (onlyOnWrite && !isWrite) return;
+      if (!(keys.length > 1)) return;
+
+      keys.forEach((key) => {
+        const { isEntry, facadeModuleId } = bundle[key];
+        if (isEntry && facadeModuleId) {
+          if (Array.isArray(extensions)) {
+            extensions.forEach((ele) => {
+              if (facadeModuleId.endsWith(ele)) {
+                delete bundle[key];
+              }
+            });
+          } else if (typeof extensions === 'string') {
+            if (facadeModuleId.endsWith(extensions)) {
+              delete bundle[key];
             }
-
-            return options
-        },
-        async generateBundle(options, bundle, isWrite) {
-            if (onlyOnWrite && !isWrite) return;
-            if (!(Object.keys(bundle).length > 1)) return;
-
-            for (const key in bundle) {
-                const { isEntry, facadeModuleId } = bundle[key];
-                if (isEntry && facadeModuleId) {
-                    if (Array.isArray(extensions)) {
-                        extensions.forEach(ele => {
-                            if (facadeModuleId.endsWith(ele)) {
-                                delete bundle[key];
-                            }
-                        });
-                    } else if (typeof extensions === 'string') {
-                        if (facadeModuleId.endsWith(extensions)) {
-                            delete bundle[key];
-                        }
-                    }
-
-                    break;
-                }
-            }
-
-            return bundle;
+          }
         }
-    };
-}
+      });
+
+      return bundle;
+    },
+  };
+};
